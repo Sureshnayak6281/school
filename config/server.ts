d447086371a4868c4f4b9c2c584a096de84f61a1
Banothu Suresh <sureshnayak6281@gmail.com>
2025-03-08 15:37:06 +0530

    Added jwtSecret for authentication

--- config/server.ts
+++ config/server.ts
@@ -2,6 +2,6 @@ export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['abu78GDzRipGZNg==','pc3lH61r1njS8yK7+B5Plw==','OB/003t3u5DroUQh5mdpHw==','cXrkKWGEDnd46b9234OIGg==']),
   },
 });
